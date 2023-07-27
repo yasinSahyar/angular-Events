50ec856091f8d059fe4b4ee7b0155a2180c77de0
urumci <nijatt23@gmail.com>
2023-07-27 17:26:34 +0300

    add angular service

--- src/app/movie.datasource.ts
+++ src/app/movie.datasource.ts
@@ -1,11 +1,11 @@
 import { Movie } from "./movie";
 
 export const Movies: Movie[] = [
-    {id: 1, name: "movie 1"},
-    {id: 2, name: "movie 2"},
-    {id: 3, name: "movie 3"},
-    {id: 4, name: "movie 4"},
-    {id: 5, name: "movie 5"}
+    {id: 1, name:"movie 1"},
+    {id: 2, name:"movie 2"},
+    {id: 3, name:"movie 3"},
+    {id: 4, name:"movie 4"},
+    {id: 5, name:"movie 5"}
     
 
 ];
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
-import { Movies } from "../movie.datasource";
 import { Movie } from "../movie";
+import { MovieService } from "../movie.service";
 
 
 @Component({
@@ -10,14 +10,26 @@ import { Movie } from "../movie";
 
 export class MoviesComponent {
     title = 'Movie List';
-    movies = Movies;
+    movies : Movie[] = [] ;
     selectedMovie: Movie | null = null; 
 
-    
+    constructor(private movieService:MovieService){}
 
+    ngOnInit(): void {
+        this.getMovies();
+        //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+        //Add 'implements OnInit' to the class. constructor dan sonra calisir anlaminda
+        
+    }
+
+    
     onSelect(movie:Movie): void {
         this.selectedMovie = movie;
 
+    }
+
+    getMovies(): void {
+        this.movies = this.movieService.getMovies();
     }
 
 
